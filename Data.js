25bc67cd2509eb2ddcb44df248ff7c9f9c9efdc0
lordArmada <108556918+lordArmada@users.noreply.github.com>
2023-03-16 23:42:30 +0100

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,38 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/images/a71787d8-2d35-40d4-aa5b-5c1ad5f65aeb.JPG",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/images/a6502d87-e012-4fee-8c73-cab7c6f60d80.JPG",
+    size: "<option>S</option>"
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/images/4ed9919a-4934-4513-b04c-840f24ac8381.JPG",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/images/2e7c97f2-a5e3-4087-94f4-c292035d91b2.JPG",
+  },
+  {
+    id: "fbjgjfhgjg",
+    name: "jean jacket",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/images/067c6f32-dcfc-495b-a14e-760751c78556.JPG",
+  },
+];
